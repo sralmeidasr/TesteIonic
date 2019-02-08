29b5a531742fabf8629cb10c4122f3c40775fbf0
Alan Almeida <sralmeida@duetoperfeito.com.br>
2019-02-08 14:15:18 -0200

    Adding SQL command

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { NewEntryPage } from '../new-entry/new-entry';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { SQLite } from '@ionic-native/sqlite/ngx';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public sqlite: SQLite) {
 
   }
 
@@ -18,4 +20,30 @@ export class HomePage {
     this.navCtrl.push(NewEntryPage);
   }
 
+  testeDb() {
+    console.log('Início do Teste DB');
+
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      console.log('BD criado');
+      
+      // Executa o comando create table
+      db.sqlBatch([
+        ["CREATE TABLE entries ( id INTEGER PRIMARY KEY AUTOINCREMENT, amount DECIMAL, description TEXT)"]
+      ]);
+      .then(() => {
+        console.log('tabelas criadas');
+      })
+      .catch((e) => {
+        console.error('erro ao executar o comando sql', JSON.stringify(e));
+      });      
+
+    })
+    .catch(() => { 
+      console.error('Error ao criar o BD.'); 
+    });
+  }
 }
